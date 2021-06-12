071ef54fe310e2696db3fe81590f9435683e27ec
William Crews <crewswh@gmail.com>
2021-06-12 12:32:38 -0400

    committed

---
+++ datapointers.sql
@@ -0,0 +1,8 @@
+-- DROP DATABASE IF EXISTS bidz_dudeDB;
+
+-- CREATE DATABASE bidz_dudeDB;
+
+-- USE bidz_dudeDB;
+
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+
